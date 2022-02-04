7191da72c864e650f33521b1c9a97dd75ca25495
pcrpmm26 <maysexrn@gmail.com>
2022-02-04 17:29:12 +0700

    first commit

---
+++ src/main/java/nvc/it/springapi/model/Review.java
@@ -0,0 +1,12 @@
+package nvc.it.springapi.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class Review {
+    private Integer star;
+    private String comment;
+
+}
